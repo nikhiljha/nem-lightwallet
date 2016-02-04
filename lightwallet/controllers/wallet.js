c8b644e9286327d507d8a104b523593e6074639b
QuantumMechanics <QuantumMechanics@users.noreply.github.com>
2016-02-04 20:16:19 +0100

    Fix [$rootScope:inprog] error on Firefox browsers

--- lightwallet/controllers/wallet.js
+++ lightwallet/controllers/wallet.js
@@ -270,54 +270,52 @@ define([
                     function unconfirmedCallback(d) {
                         // we could first check if coming tx is already in unconfirmed, but
                         // tx itself can change in case of multisig txes,, so don't do that check
-
-                        $scope.$apply(function(){
-                            $scope.unconfirmed[d.meta.hash.data] = d;
-                            $scope.unconfirmedSize = Object.keys($scope.unconfirmed).length;
-                        });
+			$timeout(function() {
+      			 $scope.unconfirmed[d.meta.hash.data] = d;
+                         $scope.unconfirmedSize = Object.keys($scope.unconfirmed).length;
+    			}, 0);
                         //console.log("unconfirmed data: ", Object.keys($scope.unconfirmed).length, d);
                         var audio = new Audio('/lightwallet/ding.ogg');
                         audio.play();
                     }
 
                     function confirmedCallback(d) {
-                        $scope.$apply(function(){
-                            delete $scope.unconfirmed[d.meta.hash.data];
+			$timeout(function() {
+      			 delete $scope.unconfirmed[d.meta.hash.data];
                             $scope.unconfirmedSize = Object.keys($scope.unconfirmed).length;
-
                             $scope.transactions.push(d);
-                        });
+    			}, 0);
                         // console.log(">> transactions data: ", d);
                         var audio = new Audio('/lightwallet/ding2.ogg');
                         audio.play();
                     }
 
                     function mosaicDefinitionCallback(d) {
-                        $scope.$apply(function(){
-                            $scope.mosaicDefinitionMetaDataPair[mosaicIdToName(d.mosaicDefinition.id)] = d;
-                            $scope.mosaicDefinitionMetaDataPairSize = Object.keys($scope.mosaicDefinitionMetaDataPair).length;
-                        });
+			$timeout(function() {
+      			 $scope.mosaicDefinitionMetaDataPair[mosaicIdToName(d.mosaicDefinition.id)] = d;
+                         $scope.mosaicDefinitionMetaDataPairSize = Object.keys($scope.mosaicDefinitionMetaDataPair).length;
+    			}, 0);
                     }
 
                     function mosaicCallback(d, address) {
-                        $scope.$apply(function(){
-                            var mosaicName = mosaicIdToName(d.mosaicId);
+			$timeout(function() {
+      			 var mosaicName = mosaicIdToName(d.mosaicId);
                              if (! (address in $scope.mosaicOwned)) {
                                 $scope.mosaicOwned[address] = {};
                              }
                             $scope.mosaicOwned[address][mosaicName] = d;
                             $scope.mosaicOwnedSize[address] = Object.keys($scope.mosaicOwned[address]).length;
-                        });
+    			}, 0);
                     }
 
                     function namespaceCallback(d, address) {
-                        $scope.$apply(function(){
-                            var namespaceName = d.fqn;
+			$timeout(function() {
+     			  var namespaceName = d.fqn;
                              if (! (address in $scope.namespaceOwned)) {
                                 $scope.namespaceOwned[address] = {};
                              }
                             $scope.namespaceOwned[address][namespaceName] = d;
-                        });
+   			 }, 0);
                     }
 
                     connector.on('errors', function(name, d) {
